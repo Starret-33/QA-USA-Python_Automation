0ae8c5566f8d5a53c2050cf45a4780eb324fb1a6
Starret-33 <sstarret1@gmail.com>
2025-02-28 20:56:05 -0600

    Helper code

--- data.py
+++ data.py
@@ -1,1 +1,10 @@
 # File with constants
+#URL from the start server button
+URBAN_ROUTES_URL = 'https://cnt-24567a3e-cedb-420c-88b1-057fe0caae74.containerhub.tripleten-services.com'
+#Description and test data as constants
+ADDRESS__FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
--- main.py
+++ main.py
@@ -1,2 +1,50 @@
+import data
+import helpers
 
 class TestUrbanRoutes:
+# Check for server connection
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print ("Connected to the Urban Routes server")
+        else:
+            print ("Cannot connect to Urban Routes. Check the server is on and still running")
+# Setup for test functions
+    def test_set_route(self):
+        #Add in S8
+        print ("function created for set route")
+        pass #Placeholder
+    def test_select_plan(self):
+        #Add in S8
+        print ("function created for select plan")
+        pass #Placeholder
+    def test_fill_phone_number(self):
+        #Add in S8
+        print ("function created for fill phone number")
+        pass #Placeholder
+    def test_fill_card(self):
+        #Add in S8
+        print ("function created for fill card")
+        pass #Placeholder
+    def test_comment_for_driver(self):
+        #Add in S8
+        print ("function created for comment for driver")
+        pass #Placeholder
+    def test_order_blanket_and_handkerchiefs(self):
+        #Add in S8
+        print ("function created for order blanket and handkerchiefs")
+        pass #Placeholder
+    def test_order_2_ice_creams(self):
+        for ice_cream in range(2): #Loop iterates twice
+            #Add in S8
+            pass #Placeholder
+        #Add in S8
+        print ("function created for order 2 ice creams")
+        pass #Placeholder
+    def test_car_search_model_appears(self):
+        #Add in S8
+        print ("function created for car search model appears")
+        pass #Placeholder
+
+
+
