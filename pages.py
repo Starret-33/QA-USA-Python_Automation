44e2f6297988759e004c1052a8a33e75c5b3c531
Starret-33 <sstarret1@gmail.com>
2025-04-15 16:06:49 -0500

    Project 8

--- pages.py
+++ pages.py
@@ -13,7 +13,6 @@ class UrbanRoutesPage:
     get_current_selected_plan = (By.XPATH, '//div[contains(text(), "Supportive")]//..')
     active_plan_card = (By.XPATH, '//div[@class = "card active"]//div[@class = "card-title"]')
     call_taxi_button = (By.XPATH, '//button[contains(text(), "Call a taxi")]')
-    time.sleep(3)
     # Filling in the phone number
     phone_number_input = (By.XPATH, '//div[@class= "np-button"]//div[contains(text(), "phone number")]')
     phone_number_label = (By.ID, "phone")
@@ -22,7 +21,6 @@ class UrbanRoutesPage:
     get_enter_code_sms_text = (By.XPATH, '//div[text()= "Enter the code from the SMS"]')
     confirm_button = (By.XPATH, '//div[text()= "Confirm"]')
     phone_number = (By.CLASS_NAME, "np-text")
-    time.sleep(5)
     # Adding a credit card
     payment_method = (By.XPATH, '//div[@class= "pp-button filled"]//div[contains(text(), "Payment method")]')
     add_card = (By.XPATH, '//div[contains(text(), "Adding a card")]')
@@ -32,16 +30,13 @@ class UrbanRoutesPage:
     validate_card_input = (By.XPATH, '//div[@class= "pp-checkbox"]')
     current_payment_method = (By.CLASS_NAME, "pp-value-text")
     close_payment_screen = (By.XPATH, '//button[contains(@class= "close-button") and contains(@class= "section-close")]')
-    time.sleep(5)
     # Writing a comment for the driver
     driver_comment = (By.XPATH, '//input[@name="comment" and @type="text"]')
     # Ordering a blanket and handkerchiefs
     click_blanket_and_handkerchiefs = (By.XPATH, '//span[@class= "slider round"][1]')
     validate_click_blanket_and_handkerchiefs = (By.XPATH, '//input[@type= "checkbox"][2]')
-    time.sleep(3)
     # Ordering 2 ice cream
     add_ice_cream = (By.XPATH, '//div[contains(@class, "counter-plus")]')
-    time.sleep(3)
     # Car search window
     select_supportive_tariff = (By.XPATH, '//div[contains(text(), "Supportive")]')
     order_button = (By.XPATH, '//button[contains(text(), "Order")]')
@@ -53,10 +48,12 @@ class UrbanRoutesPage:
     def set_from(self, from_address):
         from_field = self.driver.find_element(*self.from_field)
         from_field.send_keys(from_address)
+        time.sleep(1)
 
     def set_to(self, to_address):
         to_field = self.driver.find_element(*self.to_field)
         to_field.send_keys(to_address)
+        time.sleep(1)
 
     def get_from(self):
         return self.driver.find_element(*self.from_field).get.property('value')
@@ -66,31 +63,44 @@ class UrbanRoutesPage:
 
     def click_call_taxi_button(self):
         WebDriverWait(self.driver, 3).until(expected_conditions.presence_of_element_located(self.call_taxi_button))
-        self.driver.find_element(self.call_taxi_button).click()
+        self.driver.find_element(*self.call_taxi_button).click()
 
     def set_route(self, from_address, to_address):
         self.set_from(from_address)
         self.set_to(to_address)
+        time.sleep(2)
         self.click_call_taxi_button()
 
     def enter_phone_number(self, phone_number):
        self.driver.find_element(*self.phone_number_input).click()
+       time.sleep(2)
        self.driver.find_element(*self.phone_number_label).click()
+       time.sleep(1)
        self.driver.find_element(*self.phone_number_next_button).click()
+       time.sleep(2)
        code = retrieve_phone_code(self.driver)
        self.driver.find_element(*self.phone_number_code_label).send_keys(code)
+       time.sleep(1)
        self.driver.find_element(*self.confirm_button).click()
+       time.sleep(2)
 
     def get_phone(self):
         return self.driver.find_element(*self.phone_number).text
 
     def enter_payment_method(self, card_number, card_code):
         self.driver.find_element(*self.payment_method).click()
+        time.sleep(1)
         self.driver.find_element(*self.add_card).click()
+        time.sleep(1)
         self.driver.find_element(*self.card_number_input).send_keys(card_number)
+        time.sleep(2)
         self.driver.find_element(*self.card_code_input).send_keys(card_code)
+        time.sleep(2)
         self.driver.find_element(*self.link_button).click()
+        time.sleep(1)
         self.driver.find_element(*self.validate_card_input).click()
+        time.sleep(2)
+
 
     def click_close_payment_screen(self):
         WebDriverWait(self.driver, 3).until(expected_conditions.presence_of_element_located(self.close_payment_screen))
@@ -101,9 +111,11 @@ class UrbanRoutesPage:
 
     def driver_comment_value(self, message):
         self.driver.find_element(*self.driver_comment).send_keys(message)
+        time.sleep(2)
 
     def order_blanket_and_handkerchiefs(self):
         self.driver.find_element(*self.click_blanket_and_handkerchiefs).click()
+        time.sleep(2)
 
     def get_order_requirements_checked(self):
         return self.driver.find_element(*self.click_blanket_and_handkerchiefs).get.property('checked')
@@ -111,15 +123,18 @@ class UrbanRoutesPage:
     def click_add_ice_cream(self):
         for _ in range(2):  # Loop iterates twice
             self.driver.find_element(*self.add_ice_cream).click()
+            time.sleep(2)
 
     def search_supportive_tariff(self):
         self.driver.find_element(*self.select_supportive_tariff).click()
+        time.sleep(2)
 
     def click_order_button(self):
         self.driver.find_element(*self.order_button).click()
+        time.sleep(2)
 
     def is_car_search_modal_visible(self):
-        WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(self.car_search_modal))
+        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located(self.car_search_modal))
         return self.driver.find_element(*self.car_search_modal).is_displayed()
 
 
